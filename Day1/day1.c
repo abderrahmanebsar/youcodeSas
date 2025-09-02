35a713ef98e1000ba8c773a5d859deab88b793e4
abderrahmane <absarabd.o4@gmail.com>
2025-09-02 10:06:55 +0100

    programme en C qui va permettre d'afficher vos informations personnelles : nom, prénom, âge, sexe, et adresse email

---
+++ Day1/day1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+
+    //Challenge 1
+
+    char nom[] = "bsar";
+    char prenom[] = "abderrahmane";
+    int age = 23;
+    char email[] = "exemple@exemple.com";
+    char sexe = 'M';
+
+
+    printf("nom : %s et le prenom : %s\n",nom,prenom);
+    printf("age : %d et le sexe : %c\n",age,sexe);
+    printf("email : %s\n",email);
+
+
+}
