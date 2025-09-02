a1b96409b40eb13549057d873150099e65271bf6
abderrahmane <absarabd.o4@gmail.com>
2025-09-02 11:08:30 +0100

    un programme qui demande la vitesse en kilomètres par heure (km/h) et la transforme en mètres par seconde (m/s)

--- Day1/day1.c
+++ Day1/day1.c
@@ -5,35 +5,47 @@ int main() {
 
     //Challenge 1
 
-    char nom[] = "bsar";
-    char prenom[] = "abderrahmane";
-    int age = 23;
-    char email[] = "exemple@exemple.com";
-    char sexe = 'M';
+    // char nom[] = "bsar";
+    // char prenom[] = "abderrahmane";
+    // int age = 23;
+    // char email[] = "exemple@exemple.com";
+    // char sexe = 'M';
 
 
-    printf("nom : %s et le prenom : %s\n",nom,prenom);
-    printf("age : %d et le sexe : %c\n",age,sexe);
-    printf("email : %s\n",email);
+    // printf("nom : %s et le prenom : %s\n",nom,prenom);
+    // printf("age : %d et le sexe : %c\n",age,sexe);
+    // printf("email : %s\n",email);
 
 
-    //challenge 2
+    // //challenge 2
 
-    float c, k;
-    printf("Entrez la température en Celsius : ");
-    scanf("%f",&c);
-    k = c + 273.15;
+    // float c, k;
+    // printf("Entrez la température en Celsius : ");
+    // scanf("%f",&c);
+    // k = c + 273.15;
     
-    printf("la température en Kelvin et :%.2f\n",k);
+    // printf("la température en Kelvin et :%.2f\n",k);
 
 
     //challenge 3
 
-    float yards, km;
-    printf("Entrez la distance en kilomètres : ");
-    scanf("%f",&km);
+    // float yards, km;
+    // printf("Entrez la distance en kilomètres : ");
+    // scanf("%f",&km);
     
-    yards = km * 1093.61;
+    // yards = km * 1093.61;
 
-    printf("La distance en yards : %.2f\n", yards);
+    // printf("La distance en yards : %.2f\n", yards);
+
+    //challenge 4
+    float kmh,ms;
+
+    printf("Entrez  la vitesse en kilomètres par heure (km/h : ");
+    scanf("%f",&kmh);
+
+    ms = kmh * 0.27778;
+
+    printf("la vitesse en  seconde (m/s): %.2f\n",ms);
+
+    return 0;
 }
