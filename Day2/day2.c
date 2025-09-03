529ef5f8aab8550969b35921db81f2ef95c80e4d
abderrahmane <absarabd.o4@gmail.com>
2025-09-03 09:57:02 +0100

    un programme C qui demande à l'utilisateur le nombre d'éléments d'un tableau, puis demande à l'utilisateur de saisir ces éléments. Affichez ensuite les éléments du tableau.

--- Day2/day2.c
+++ Day2/day2.c
@@ -3,12 +3,32 @@
 int main() {
 
     //array 
-    int num[] = {1,2,3,4,5,6};
-    int len = sizeof(num) / sizeof(num[0]);
+    //challenge 1
+    // int num[] = {1,2,3,4,5,6};
+    // int len = sizeof(num) / sizeof(num[0]);
 
-    for ( int  i = 0; i < len; i++)
+    // for ( int  i = 0; i < len; i++)
+    // {
+    //     printf("%d\n",num[i]);
+    // }
+
+
+    //challenge 2
+
+    int len;
+    printf("enter the size of the array : ");
+    scanf("%d",&len);
+
+    int arr[len];
+
+    for (int i = 0; i < len;i++){
+        printf("enter the number  %d : ",i +1);
+        scanf("%d",&arr[i]);
+    }
+
+   for ( int  i = 0; i < len; i++)
     {
-        printf("%d\n",num[i]);
+        printf("%d\n",arr[i]);
     }
     
 }
