b6b5942a3c9910b356acce4b16a933021b01d757
abderrahmane <absarabd.o4@gmail.com>
2025-09-04 11:15:54 +0100

    un programme C qui calcule et affiche la longueur d'une chaîne de caractères

--- Day3/day3.c
+++ Day3/day3.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    //challnege
+    //challnege  1
+    // char str[50];
+
+    // printf("enter a string : ");
+    // scanf("%s",str);
+
+    // printf("the string you enter is : %s",str);
+
+
+    //challenge 2
+
     char str[50];
+    int length = 0;
 
     printf("enter a string : ");
     scanf("%s",str);
 
-    printf("the string you enter is : %s",str);
+    for (int i = 0; str[i] != '\0'; i++)
+    {   
+        length++;
+    }
+
+    printf("the length of the %s is : %d",str,length);
+    
+
 }
