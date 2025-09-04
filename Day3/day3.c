2ef8831881c2aa18a108d82e1c61f66aebfc4a5c
abderrahmane <absarabd.o4@gmail.com>
2025-09-04 11:07:59 +0100

    un programme C qui lit une chaîne de caractères entrée par l'utilisateur et l'affiche à l'écran.

---
+++ Day3/day3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    //challnege
+    char str[50];
+
+    printf("enter a string : ");
+    scanf("%s",str);
+
+    printf("the string you enter is : %s",str);
+}
