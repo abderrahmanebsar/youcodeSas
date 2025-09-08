d99ad27e18bda81f390c23d2b01adf608b7f14d6
abderrahmane <absarabd.o4@gmail.com>
2025-09-08 09:45:20 +0100

    un programme C qui lit deux chaînes de caractères et les concatène en une seule chaîne. Affichez la chaîne résultante.

--- Day3/day3.c
+++ Day3/day3.c
@@ -30,30 +30,30 @@ int main() {
 
     char str1[100];
     char str2[100];
-    int i =0, j=0;
+    int i = 0, j = 0;
 
 
     printf("enter the first string : ");
     scanf("%s",str1);
 
+
     printf("enter the second string : ");
     scanf("%s",str2);
 
-    while (str1[i] != '\0')
-    {
+
+    while (str1[i] != '\0'){
         i++;
     }
 
-
-    while (str2[j] != '\0')
-    {
+    while (str2[j] != '\0') {
         str1[i] = str2[j];
         i++;
         j++;
     }
     
+
     str1[i] = '\0';
 
-    printf("%s",str1);
+    printf("the marges string is : %s",str1);
 
-}
+}   
