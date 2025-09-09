1e6372ea5139ab71b969cfc77f4ba66346665150
abderrahmane <absarabd.o4@gmail.com>
2025-09-09 11:05:18 +0100

    un programme C qui implémente l'algorithme de tri à bulles pour trier un tableau d'entiers. Affichez le tableau avant et après le tri.

---
+++ Algos/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main(){
+    //Challenge 1 : Tri à Bulles
+
+    // int arr[10] = {1,4,7,33,99,9,100,6,76,34};
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     for (int j = i; j < 10; j++)
+    //     {
+    //         if(arr[j] > arr[j + 1]){
+    //             int temp = arr[j];
+    //             arr[j] = arr[j + 1];
+    //             arr[j + 1] = temp;
+    //         }
+    //     }
+    
+    // }
+
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d \t",arr[i]);
+    // }
+    
+
+ 
+
+    
+}
