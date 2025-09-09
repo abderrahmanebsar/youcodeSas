8a6ddd7b60a1e66398dddd01742091e44c9c1722
abderrahmane <absarabd.o4@gmail.com>
2025-09-09 14:32:15 +0100

    un programme C qui implémente l'algorithme de tri par insertion pour trier un tableau d'entiers. Affichez le tableau avant et après le tri.

--- Algos/main.c
+++ Algos/main.c
@@ -27,6 +27,32 @@ int main(){
     
 
  
+    //Challenge 2 : Tri par Insertion
+
+    int arr[9] = {6,1,7,4,2,9,8,5,3};
+    int temp;
+
+
+    for (int i = 1; i < 9; i++)
+    {
+        temp = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        
+    }
+    
+    
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d \t",arr[i]);
+    }
+    
 
     
 }
